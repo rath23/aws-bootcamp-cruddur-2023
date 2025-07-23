7cf8a27b1ccbd597650e75ab0fded942506099a7
Muneer Ahmad <sayyadmuneerahmad786@gmail.com>
2025-07-23 05:00:47 +0000

    added rollbar code and env

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -52,8 +52,55 @@ tracer = trace.get_tracer(__name__)
 # xray_url = os.getenv("AWS_XRAY_URL")
 # xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
+#RoolBAr--------------
+
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
+
 
 app = Flask(__name__)
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+
+# with app.app_context():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token ,
+#         # environment name - any string, like 'production' or 'development'
+#         'flasktest',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
+
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+    
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 
 # XRayMiddleware(app, xray_recorder)
